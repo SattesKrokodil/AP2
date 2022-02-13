376cce7bc99302989c1d884ca59c9645441fc9b2
Sattes <maximilian.zmek@gmail.com>
2022-02-13 23:04:44 +0100

    added dummy origin with mana and one dash skill added room item textures for dungeon system

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -5,6 +5,38 @@ console.info('Hello, World! (You will only see this line once in console, during
 onEvent('item.registry', event => {
 	// Register new items here
 	// event.create('example_item').displayName('Example Item')
+	event.create('raw_resin', item => {
+      item.displayName('Raw Resin').tooltip("This is a test item.")
+	})
+	
+		event.create('sw_connector', item => {
+      item.displayName('SW Connector').tooltip("This is a test item.")
+	})
+			event.create('cross_connector', item => {
+      item.displayName('CROSS Connector').tooltip("This is a test item.")
+	})
+			event.create('ew_connector', item => {
+      item.displayName('EW Connector').tooltip("This is a test item.")
+	})
+			event.create('ne_connector', item => {
+      item.displayName('NE Connector').tooltip("This is a test item.")
+	})
+			event.create('ns_connector', item => {
+      item.displayName('NS Connector').tooltip("This is a test item.")
+	})
+			event.create('nw_connector', item => {
+      item.displayName('NW Connector').tooltip("This is a test item.")
+	})
+			event.create('room', item => {
+      item.displayName('Room').tooltip("This is a test item.")
+	})
+			event.create('se_connector', item => {
+      item.displayName('SE Connector').tooltip("This is a test item.")
+	})
+	
+		event.create('resin_flask', item => {
+      item.displayName('Resin').tooltip("Nom nom")
+	})
 })
 
 onEvent('block.registry', event => {
